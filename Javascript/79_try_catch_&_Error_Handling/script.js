d6d98ba4c07b7a695fc3ebffb93e96424193de56
unstoppableayush <ayushkum884@gmail.com>
2023-12-31 21:38:41 +0530

    try catch and finally done

--- Javascript/79_try_catch_&_Error_Handling/script.js
+++ Javascript/79_try_catch_&_Error_Handling/script.js
@@ -1,1 +1,36 @@
-let a = prompt("Enter the ")
+let a = prompt("Enter the first number");
+let b = prompt("Enter the second number");
+
+// let sum = a+b;
+// concat the two values
+
+
+if(isNaN(a) || isNaN(b)){
+    throw SyntaxError("Sorry this is not allowed");
+}
+
+let sum = parseInt(a)+parseInt(b);
+// converts to integer 
+
+function main(){
+
+    let x =1 ;
+    try{
+        console.log("The sum is ",sum*x);
+        return true;
+    }
+    catch(error){
+        console.log("Error aa gaya bhai");
+    }
+    finally{
+        console.log("files are being closed and db connection is being closed");
+    }
+
+    // console.log("files are being closed and db connection is being closed");
+    // not work if we return after try
+    // if we have finally back then after return it will execute
+
+}
+
+let c = main();
+
