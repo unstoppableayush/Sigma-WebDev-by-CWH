c6008efdefa8047369c09da17bef34f99fbd7cad
unstoppableayush <ayushkum884@gmail.com>
2024-01-04 23:39:03 +0530

    seekbar working properly

---
+++ Javascript/83_Javascript_Interview_Questions/tempCodeRunnerFile.js
@@ -0,0 +1,18 @@
+let ele = [ 2, 3 ,4 , 5, 5 , 6 ,7];
+
+// for(let i = 0 ; i< ele.length ; i++){
+
+//     try{
+
+//         if(ele[i] == ele[i-1]){
+//             console.log("Consequtive Elements are Same");
+//             throw i;
+//         }
+//         else
+//         ele[i] = ele[i] * 2;
+//     }
+//     catch(index){
+//         ele[i] = ele[i];
+//     }
+    
+// }
