5d2e345c9199135e607ded04175ce74e859ca626
unstoppableayush <ayushkum884@gmail.com>
2024-01-10 22:23:11 +0530

    ejs tutorial done

---
+++ Backend/91_Clear_the_Clutter/index.js
@@ -0,0 +1,20 @@
+/*
+You have to write a Node.js program to clear clutter inside of
+a directory and organize the contents of that directory
+into different folders
+
+for examples , these files become:
+
+1.name.jpg
+2.name.png
+3.this.pdf
+4.harry.zip
+5.Rohan.zip
+6.Cat.jpg
+7.harry.pdf
+
+this:
+jpg/name.jpg , jpg/cat.jpg
+png/name.png 
+zip/harry.zip zip/Rohan.zip
+*/
---
+++ Backend/92_ejs_Template_Engine_in_Express/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+// const ejs = require('ejs')
+const app = express()
+const port = 3000
+
+app.set('view engine' , 'ejs')
+
+app.get('/' , (req, res)=>{
+    let siteName = "Addidas"
+    let searchText = "Search Now"
+    let arr = ["Hey , 54 , 65"]
+    res.render("index" ,{siteName:siteName , searchText:searchText ,arr});
+})
+
+app.get('/blog/:slug' , (req , res)=>{
+    let blogTitle = "Adidas why and when"
+    let blogContent = "It's a very good brand"
+    res.render("blogpost" ,{blogTitle:blogTitle , blogContent:blogContent});
+    
+})
+app.listen(port , ()=>{
+    console.log(`Example app listening on port ${port}`)
+})
