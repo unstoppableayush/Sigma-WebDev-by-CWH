f0364fa686fc0c40c0ebfa5e6dadff90dce72241
unstoppableayush <ayushkum884@gmail.com>
2024-02-08 00:31:31 +0530

    handling events in react done

--- ReactJS/112_Handling_Events_in_React/src/App.jsx
+++ ReactJS/112_Handling_Events_in_React/src/App.jsx
@@ -5,6 +5,8 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [name, setName] = useState("Ayush")
+  const [form, setform] = useState({email:"",phone:""})
 
   const handleClick = () =>{
     alert("Hey I am clicked")
@@ -14,6 +16,16 @@ function App() {
     alert("Hey I am mouse over")
   }
 
+  const handleChange = (e)=>{
+    setName(e.target.value)
+  }
+
+ // using one handleChange we are changing the value of different input text
+  const handleChangeBtn = (e)=>{
+    setform({...form, [e.target.name]:e.target.value})
+    console.log(form)
+  }
+
   return (
     <>
       
@@ -21,6 +33,14 @@ function App() {
       <div className="button">
         <button onClick={handleClick}>Click me</button>
       </div>
+
+      <div className="red" onMouseOver={handleMouseOver}>I am a red div</div>
+
+      <input type="text" value={name} onChange={handleChange} /><br /><br />
+      <input type="text" value={form.email ? form.email : ""} name='email' onChange={handleChangeBtn}/>
+      <input type="text" value={form.phone ? form.phone : ""} name='phone' onChange={handleChangeBtn}/>
+      
+      {/* if form.email is present the keep that otherwise use blank */}
        
     </>
   )
