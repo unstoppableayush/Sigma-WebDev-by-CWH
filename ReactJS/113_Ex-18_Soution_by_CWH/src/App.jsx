c42d147a7d2c96f2fd84796b8dd0f25527103ff8
unstoppableayush <ayushkum884@gmail.com>
2024-02-09 22:58:28 +0530

    execise complted by CWH

--- ReactJS/113_Ex-18_Soution_by_CWH/src/App.jsx
+++ ReactJS/113_Ex-18_Soution_by_CWH/src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState,useEffect } from 'react'
+import Navbar from './components/Navbar'
 import './App.css'
 
 function App() {
   const [cards, setCards] = useState([])
   
-  const fetchData = async()=>{
-    let a = await fetch('https://jsonplaceholder.typicode.com/posts/')
+  const fetchData = async ()=>{
+    let a = await fetch('https://jsonplaceholder.typicode.com/posts')
     let data = await a.json()
     setCards(data)
     console.log(data)
@@ -20,7 +19,7 @@ function App() {
 
   return (
     <>
-    <h1>Helllo</h1>
+     <Navbar/>
      <div className="container">
       {cards.map((card)=>{
         return <div key={card.id} className="card">
