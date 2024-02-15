4c4a580bf3a9affeaf92a7853eada5a369ec07e4
unstoppableayush <ayushkum884@gmail.com>
2024-02-15 23:42:33 +0530

    Frontend done next backend

--- ReactJS/114_TodoList_App/src/app.jsx
+++ ReactJS/114_TodoList_App/src/app.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'preact/hooks'
 import preactLogo from './assets/preact.svg'
 import viteLogo from '/vite.svg'
+import Navbar from './components/Navbar'
 
 
 export function App() {
@@ -8,11 +9,29 @@ export function App() {
 
   return (
     <>
-      <div className="continer">
-    <div className="bg-red-600">
-      Hey I am red
-    </div>
+    <Navbar/>
+      <div className="container mx-auto my-5 rounded-xl p-5 bg-violet-100 min-h-[80vh]">
+        <div className="addTodo">
+          <h2 className='text-lg font-bold'>Add a Todo</h2>
+          <input type="text" className="w-80"/>
+          <button className='bg-violet-800 hover:bg-violet-950 p-2 font-bold py-1 text-sm text-white rounded-md m-6'>Add</button>
+        </div>
+        
+          
+          <h2 className='text-xl font-bold'>Your Todos</h2>
+          <div className="todos">
+            <div className="todo flex">
+
+              <div className="text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, ea!</div>
+              <div className="buttons">
+                <button className='bg-violet-800 hover:bg-violet-950 p-2 font-bold py-1 text-sm text-white rounded-md mx-1'>Edit</button>
+                <button className='bg-violet-800 hover:bg-violet-950 p-2 font-bold py-1 text-sm text-white rounded-md mx-1'>Delete</button>
+              </div>
+
+            </div>
+          </div>
       </div>
+      
     </>
   )
 }
---
+++ ReactJS/114_TodoList_App/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className='flex justify-between bg-blue-800 text-white py-3'>
+      <div className="logo">
+        <span className='font-bold text-xl mx-9'>iTask</span>
+      </div>
+      <ul className="flex gap-8 mx-5">
+          <li className="cursor-pointer hover:font-bold transition-all duration-150">Home</li>
+          <li  className="cursor-pointer hover:font-bold transition-all">Your Task</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
