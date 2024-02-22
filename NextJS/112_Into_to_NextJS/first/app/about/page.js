1fa60ee53df56aa86e3323c3bb5b360801483977
unstoppableayush <ayushkum884@gmail.com>
2024-02-23 02:48:32 +0530

    intro to next.js and file routing basics

---
+++ NextJS/112_Into_to_NextJS/first/app/about/page.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+    <h1 className='text-xl'>Problems solved by Next.JS</h1>
+    <ul>
+      <li>Full Stack Solution</li>
+      <li>File based Routinh</li>
+      <li>Addditional feature like router from next/navigatio</li>
+      <li>Optimized rendering</li>
+    </ul>
+    </div>
+  )
+}
+
+export default page
---
+++ NextJS/112_Into_to_NextJS/first/app/contact/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>I am Contact</div>
+  )
+}
+
+export default page
