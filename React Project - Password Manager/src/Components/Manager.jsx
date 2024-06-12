0046c680cd1adfe66f954a5ac895825ec510a59f
unstoppableayush <ayushkum884@gmail.com>
2024-06-12 20:30:40 +0530

    Password saved into localstorage

--- React Project - Password Manager/src/Components/Manager.jsx
+++ React Project - Password Manager/src/Components/Manager.jsx
@@ -1,15 +1,39 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const Manager = () => {
   const ref = useRef();
+  const [form, setform] = useState({ site: "", username: "", password: "" });
+  const [passwordArray, setPasswordArray] = useState([]);
+
+  useEffect(() => {
+    let passwords = localStorage.getItem("passwords");
+    let passwordArray;
+    if (passwords) {
+      setPasswordArray(JSON.parse(passwords));
+    }
+  }, []);
+
+
   const showPassword = (params) => {
-    alert("Show th password");
+    alert("Show the password");
     if (ref.current.src.includes("hide.png")) {
       ref.current.src = "eye.png";
     } else {
       ref.current.src = "hide.png";
     }
   };
+
+  const savePassword = () => {
+    console.log(form)
+    setPasswordArray([...passwordArray, form]);
+    localStorage.setItem("password", JSON.stringify([...passwordArray, form]))
+    console.log(passwordArray)
+  }
+
+  const handleChange = (e) => {
+    setform({ ...form, [e.target.name]: e.target.value });
+  };
+
   return (
     <div className="absolute top-0 -z-10 h-full w-full bg-green-50">
       <div className="absolute bottom-auto left-auto right-0 top-0 h-[500px] w-[500px] -translate-x-[30%] translate-y-[20%] rounded-full  opacity-50 blur-[80px]"></div>
@@ -26,18 +50,27 @@ const Manager = () => {
         <div className=" flex flex-col p-4 gap-8 text-black items-center">
           <input
             placeholder="Enter Website URL"
+            value={form.site}
+            onChange={handleChange}
+            name="site"
             className="rounded-full border border-green-500 w-full text-black py-1 p-4"
             type="text"
           />
           <div className="flex w-full gap-8 ">
             <input
               placeholder="Enter Username"
+              value={form.username}
+              onChange={handleChange}
+              name="username"
               className="rounded-full border border-green-500 w-full text-black py-1 p-4"
               type="text"
             />
             <div className="relative">
               <input
                 placeholder="Enter Password"
+                value={form.password}
+                onChange={handleChange}
+                name="password"
                 className="rounded-full border border-green-500 w-full text-black py-1 p-4"
                 type="text"
               />
@@ -49,7 +82,10 @@ const Manager = () => {
               </span>
             </div>
           </div>
-          <button className="flex justify-center items-center bg-green-500 hover:bg-green-600 rounded-full px-8 py-2 w-fit gap-2 hover:border border-green-900 ">
+          <button
+            onClick={savePassword}
+            className="flex justify-center items-center bg-green-500 hover:bg-green-600 rounded-full px-8 py-2 w-fit gap-2 hover:border border-green-900 "
+          >
             <lord-icon
               src="https://cdn.lordicon.com/jgnvfzqg.json"
               trigger="hover"
